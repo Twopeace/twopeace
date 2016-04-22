0b616443c29291de912d3cfb679a9406dc7dc864
Scotchard22 <scotchard@aol.com>
2016-04-21 23:56:52 -0400

    added to contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,12 +1,14 @@
 class Contact < MailForm::Base
-  attribute :name,     :validate => true
-  attribute :email,    :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,  :validate => true
-  attribute :nickname, :captcha => true
+  attribute :first_name,    :validate => true
+  attribute :last_name,     :validate => true
+  attribute :email,         :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :phone_number,   :validate => true
+  attribute :message,       :validate => true
+  attribute :nickname,      :captcha => true
 
   def headers
     {
-      :subject => "Contact Form",
+      :subject => "Twopeace Contact Form",
       :to => "scotchard@twopeace.co",
       :from => %("#{name}" <#{email}>)
     }
